dc39e199cb8dcc0200726eef9a6101847684889f
Achigyus <gyuseachi@yahoo.com>
2025-03-22 22:28:53 +0100

    hooked up frontend and backend

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 3000
-const CLIENTURL = process.env.CLIENTURL || 'http://127.0.0.1:5500'
+const CLIENTURL = process.env.CLIENTURL || 'https://frontend-quiz-app-achigyus.netlify.app/'
 
 // add cors
 app.use(cors({ origin: CLIENTURL }));
